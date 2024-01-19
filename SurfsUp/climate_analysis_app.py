260dea09ed45426e663b3318181d96a6a4e939f0
Pooja14n <pooja.14.n@gmail.com>
2024-01-18 19:20:40 -0500

    Update climate_analysis_app.py

--- SurfsUp/climate_analysis_app.py
+++ SurfsUp/climate_analysis_app.py
@@ -53,8 +53,8 @@ def homepage():
             f"/api/v1.0/precipitation <br>"
             f"/api/v1.0/stations <br>"
             f"/api/v1.0/tobs <br>"
-            f"/api/v1.0/start <br>"
-            f"/api/v1.0/start/end"
+            f"/api/v1.0/<start> <br>"
+            f"/api/v1.0/<start>/<end>"
     )
     
     
